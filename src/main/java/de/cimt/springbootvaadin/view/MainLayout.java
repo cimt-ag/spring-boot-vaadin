d7df9fdb7e1cdfe447e7aa85c72c7b3bffeafc88
Thomas Peetz <thomas.peetz@ingenieurbuero-peetz.de>
2025-03-13 18:12:09 +0100

    Add MainLayout

---
+++ src/main/java/de/cimt/springbootvaadin/view/MainLayout.java
@@ -0,0 +1,13 @@
+package de.cimt.springbootvaadin.view;
+
+@Slf4j
+class MainLayout exteds AppLayout {
+    public MainLayout() {
+        createHeader("Spring Boot Vaadin");
+        createDrawer();
+    }
+
+    public void createDrawer() {
+    }
+}
+
